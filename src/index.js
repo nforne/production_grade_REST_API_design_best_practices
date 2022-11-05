ed0fed94e4213c13c21b23840e850d4909b99a5d
nforne <mnforne@gmail.com>
2022-11-04 23:00:38 -0400

    add createNewWorkout and uuid

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
 app.use('/api/v1/workouts', v1WorkoutRouter);
 
