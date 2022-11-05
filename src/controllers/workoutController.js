2f76907d6b3a4d6f95ab78157ba7ffc5c84f8e53
nforne <mnforne@gmail.com>
2022-11-05 01:24:51 -0400

    add proper error handling at all levels with standard http response codes

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,24 +1,42 @@
 const workoutService = require("../services/workoutService");
 
 const getAllWorkouts = (req, res) => {
-  const allWorkouts = workoutService.getAllWorkouts();
-  res.status(200).send({status: "OK", data: allWorkouts });
+  try {
+    const allWorkouts = workoutService.getAllWorkouts();
+    res.status(200).send({ status: "OK", data: allWorkouts });
+  } catch (error) {
+    res.status(error ? error.status : 500)
+       .send({ status: "FAILED", data: { error: error ? error.message : error} });
+  }
 };
 
 const getOneWorkout = (req, res) => {
-  console.log('=>1', req.params) // ---------------
-  console.log('=>2', req.body) // ---------------
-
   const { params: { workoutId },} = req;
-  if (!workoutId) return;
-  const workout = workoutService.getOneWorkout(workoutId);
-  res.status(200).send({ status: "OK", data: workout });
+  if (!workoutId) {
+    res.status(400)
+       .send({
+          status: "FAILED",
+          data: { error: "Parameter ':workoutId' can not be empty" },
+        });
+  }
+  try {
+    const workout = workoutService.getOneWorkout(workoutId);
+    res.status(200).send({ status: "OK", data: workout });
+  } catch (error) {
+    res.status(error ? error.status : 500)
+       .send({ status: "FAILED", data: { error: error ? error.message : error} });
+  }
 };
 
 const createNewWorkout = (req, res) => {
   const { body } = req;
   if (!body.name || !body.mode || !body.equipment || !body.exercises || !body.trainerTips) {
-    return; // better yet, use express-validator
+    res.status(400)
+       .send({
+          status: "FAILED",
+          data: {error:"One of the following keys is missing or is empty in request body: 'name', 'mode', 'equipment', 'exercises', 'trainerTips'",},
+        });
+    return;
   }
   const newWorkout = {
     name: body.name,
@@ -27,22 +45,50 @@ const createNewWorkout = (req, res) => {
     exercises: body.exercises,
     trainerTips: body.trainerTips,
   };
-  const createdWorkout = workoutService.createNewWorkout(newWorkout);
-  res.status(201).send({ status: "OK", data: createdWorkout });
+  try {
+    const createdWorkout = workoutService.createNewWorkout(newWorkout);
+    res.status(201).send({ status: "OK", data: createdWorkout });
+  } catch (error) {
+    res.status(error ? error.status : 500)
+       .send({ status: "FAILED", data: { error: error ? error.message : error } });
+  }
 };
 
 const updateOneWorkout = (req, res) => {
   const { body, params: { workoutId },} = req;
-  if (!workoutId) return;
-  const updatedWorkout = workoutService.updateOneWorkout(workoutId, body);
-  res.send({ status: "OK", data: updatedWorkout });
+  if (!workoutId) {
+    res.status(400)
+       .send({
+          status: "FAILED",
+          data: { error: "Parameter ':workoutId' can not be empty" },
+        });
+  }
+  try {
+    const updatedWorkout = workoutService.updateOneWorkout(workoutId, body);
+    res.status(200).send({ status: "OK", data: updatedWorkout });
+  } catch (error) {
+    res
+      .status(error ? error.status : 500)
+      .send({ status: "FAILED", data: { error: error ? error.message : error} });
+  }
 };
 
 const deleteOneWorkout = (req, res) => {
-  const { params: { workoutId },} = req;
-  if (!workoutId) return;
-  workoutService.deleteOneWorkout(workoutId);
-  res.status(204).send({ status: "OK" });
+  const {params: { workoutId },} = req;
+  if (!workoutId) {
+    res.status(400)
+       .send({
+          status: "FAILED",
+          data: { error: "Parameter ':workoutId' can not be empty" },
+        });
+  }
+  try {
+    workoutService.deleteOneWorkout(workoutId);
+    res.status(204).send({ status: "OK" });
+  } catch (error) {
+    res.status(error ? error.status : 500)
+       .send({ status: "FAILED", data: { error: error ? error.message : error } });
+  }
 };
 
 module.exports = {
@@ -51,4 +97,5 @@ module.exports = {
   createNewWorkout,
   updateOneWorkout,
   deleteOneWorkout,
+  // getRecordsForWorkout,
 };
